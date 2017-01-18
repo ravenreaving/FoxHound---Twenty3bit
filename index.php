ca41085be79ad5cdaacc2d288c91643c90355590
RAVENREAVING\RavenReaving <ravenirvingreaving@gmail.com>
2017-01-18 12:35:17 -0800

    Dashboard - Index

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>        
+        <!-- META SECTION -->
+        <title>Foxhound - Twenty3bit</title>            
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        
+        <link rel="icon" href="favicon.png" type="image/x-icon" />
+        <!-- END META SECTION -->
+        
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
+        <!-- EOF CSS INCLUDE -->                   
+		
+		  <!-- GA API SCRIPTS --> 
+
+		
+    </head>
+    <body>
+	<form action="index.php" method="POST">
+        <!-- START PAGE CONTAINER -->
+        <div class="page-container">
+            
+            <!-- START PAGE SIDEBAR -->
+            <div class="page-sidebar">
+                <!-- START X-NAVIGATION -->
+                <ul class="x-navigation">
+                    <li class="xn-logo">
+                        <a href="index.html">FoxHound</a>
+                        <a href="#" class="x-navigation-control"></a>
+                    </li>
+                 <li class="xn-profile">
+                        <a href="#" class="profile-mini">
+                            <img src="assets/images/users/no-image.jpg" alt="Administrator"/>
+                        </a>
+                        <div class="profile">
+                            <div class="profile-image">
+                                <img src="assets/images/users/no-image.jpg" alt="Administrator"/>
+                            </div>
+                            <div class="profile-data">
+                                <div class="profile-data-name">Raven Reaving</div>
+                                <div class="profile-data-title">Administrator</div>
+                            </div>
+                          
+                        </div>                                                                        
+                    </li>
+					
+				   <li class="xn-title">Manage Account</li>
+                  <li class="xn-openable">
+                        <a href="#"><span class="fa fa-cogs"></span> <span class="xn-text">Settings</span></a>
+                        <ul>
+						
+                           <li><a href="#"><span class="fa fa-key"></span>Password</a></li>
+                        </ul>
+                    </li>                  
+                   
+				   
+                    <li class="xn-title">Navigation</li>
+                  <li class="xn-openable">
+                        <a href="#"><span class="fa fa-dashboard"></span> <span class="xn-text">Dashboards</span></a>
+                        <ul>
+							<li><a href="#"><span class="fa fa-area-chart"></span>Google Analytics</a></li>
+                           <li><a href="#"><span class="fa fa-line-chart"></span>GTMetrix</a></li>
+                        </ul>
+                    </li>                  
+                   
+                   
+              
+                    
+                </ul>
+                <!-- END X-NAVIGATION -->
+            </div>
+            <!-- END PAGE SIDEBAR -->
+            
+            <!-- PAGE CONTENT -->
+            <div class="page-content">
+                
+                <!-- START X-NAVIGATION VERTICAL -->
+                <ul class="x-navigation x-navigation-horizontal x-navigation-panel">
+                    <!-- TOGGLE NAVIGATION -->
+                    <li class="xn-icon-button">
+                        <a href="#" class="x-navigation-minimize"><span class="fa fa-dedent"></span></a>
+                    </li>
+                    <!-- END TOGGLE NAVIGATION -->
+                    <!-- SEARCH -->
+                    <li class="xn-search">
+                        <form role="form">
+                            <input type="text" name="search" placeholder="Search..."/>
+                        </form>
+                    </li>   
+                    <!-- END SEARCH -->  
+					
+					
+                    <!-- Sign out --> 
+                         <li class="pull-right"><a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span> Sign Out</a></li> 
+                    
+					<!-- End Sign out --> 					
+                          
+                   
+                  
+                 
+                </ul>
+                <!-- END X-NAVIGATION VERTICAL -->                     
+
+                <!-- START BREADCRUMB -->
+                <ul class="breadcrumb">
+                    <li><a href="index.html">FoxHound</a></li>                    
+                    <li class="active">Dashboard</li>
+                </ul>
+                <!-- END BREADCRUMB -->                       
+                
+                <!-- PAGE CONTENT WRAPPER -->
+                <div class="page-content-wrap">
+                    
+					
+					<div class="row">
+					<div class="col-md-6">
+					 <!-- START WIDGET CLOCK -->
+                            <div class="widget widget-danger widget-padding-sm">
+                                <div class="widget-big-int plugin-clock">00:00</div>                            
+                                <div class="widget-subtitle plugin-date">Loading...</div>
+                                                         
+                                <div class="widget-buttons widget-c3">
+                                    <div class="col">
+                                        <a href="#"><span class="fa fa-clock-o"></span></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="#"><span class="fa fa-bell"></span></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="#"><span class="fa fa-calendar"></span></a>
+                                    </div>
+                                </div>                            
+                            </div>                        
+                            <!-- END WIDGET CLOCK -->
+                            
+					
+					</div>
+					<div class="col-md-6">
+                            
+                            <!-- START WIDGET REGISTRED -->
+                            <div class="widget widget-default widget-item-icon">
+                                <div class="widget-item-left">
+                                    <span class="fa fa-user"></span>
+                                </div>
+                                <div class="widget-data">
+								<p>COMING SOON!</p>
+                                    <!--<div class="widget-int num-count">375</div>
+                                    <div class="widget-title">Registred users</div>
+                                    <div class="widget-subtitle">On your website</div>-->
+                                </div>
+                                                        
+                            </div>                            
+                            <!-- END WIDGET REGISTRED -->
+                            
+                        </div>
+					
+					</div>
+					
+                                 
+                    <div class="row">
+				
+				
+				<!-- Widget GTMETRIX -->
+						<div class="col-md-6">
+					
+					     <div class="panel panel-default">                            
+                                <div class="panel-body panel-body-image">
+                                    <img src="assets/images/googleanalytics.jpg" alt="Ocean"/>
+                                     <p class="panel-body-inform">
+                                        <span class="fa fa-area-chart"></span>
+                                    </p>
+                                </div>
+                                <div class="panel-body">
+                                    <h3>Google Analytics &mdash; Web Analytics</h3>
+                                    <p>Google Analytics is a freemium web analytics service offered by Google that tracks and reports website traffic.</p>
+                                </div>
+								
+								<div class="row">
+                                 <div class="panel-footer">
+								 <div class="col-md-6 pull-left">
+                                    <input type="submit" class="btn btn-info" id="btngoogle" name="btngoogle" value="Goto Dashboard">
+									</div>
+									<label for="btngoogle" class="pull-right">Analytics Dashboard</label>
+                                </div></div>
+                            </div>
+		
+					</div>
+					<!-- end GTMETRIX -->
+			
+			<!-- Widget GTMETRIX -->
+						<div class="col-md-6">
+					
+					     <div class="panel panel-default">                            
+                                <div class="panel-body panel-body-image">
+                                    <img src="assets/images/gtmetrix.jpg" alt="Ocean"/>
+                                    <p class="panel-body-inform">
+                                        <span class="fa fa-line-chart"></span>
+                                    </p>
+                                </div>
+                                <div class="panel-body">
+                                    <h3>GTMetrix &mdash; Site Performance</h3>
+                                    <p>GTMetrix is one of the tools that we have found ourselves using more and more to get detailed reports about our site's performance.</p>
+                                </div>
+								
+								<div class="row">
+                                 <div class="panel-footer">
+								 <div class="col-md-6 pull-left">
+                                    <input type="submit" class="btn btn-info" id="btngtmetrix" name="btngtmetrix" value="Goto Dashboard">
+									</div>
+									<label for="btngetmetrix" class="pull-right">GTMetrix Dashboard</label>
+                                </div></div>
+                            </div>
+		
+					</div>
+					<!-- end GTMETRIX -->
+
+			
+					
+					
+                  </div>
+                    
+                   
+                   
+				   
+				   
+                </div>
+                <!-- END PAGE CONTENT WRAPPER -->                                
+            </div>            
+            <!-- END PAGE CONTENT -->
+        </div>
+        <!-- END PAGE CONTAINER -->
+
+        <!-- MESSAGE BOX-->
+        <div class="message-box animated fadeIn" data-sound="alert" id="mb-signout">
+            <div class="mb-container">
+                <div class="mb-middle">
+                    <div class="mb-title"><span class="fa fa-sign-out"></span> Log <strong>Out</strong> ?</div>
+                    <div class="mb-content">
+                        <p>Are you sure you want to log out?</p>                    
+                        <p>Press No if youwant to continue work. Press Yes to logout current user.</p>
+                    </div>
+                    <div class="mb-footer">
+                        <div class="pull-right">
+                            <a href="pages-login.html" class="btn btn-success btn-lg">Yes</a>
+                            <button class="btn btn-default btn-lg mb-control-close">No</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END MESSAGE BOX-->
+
+		
+
+	</form>
+		
+		
+        <!-- START PRELOADS -->
+        <audio id="audio-alert" src="audio/alert.mp3" preload="auto"></audio>
+        <audio id="audio-fail" src="audio/fail.mp3" preload="auto"></audio>
+        <!-- END PRELOADS -->                  
+        
+    <!-- START SCRIPTS -->
+        <!-- START PLUGINS -->
+        <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>        
+        <!-- END PLUGINS -->
+
+        <!-- START THIS PAGE PLUGINS-->        
+        <script type='text/javascript' src='js/plugins/icheck/icheck.min.js'></script>        
+        <script type="text/javascript" src="js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js"></script>
+        <script type="text/javascript" src="js/plugins/scrolltotop/scrolltopcontrol.js"></script>
+        
+        <script type="text/javascript" src="js/plugins/morris/raphael-min.js"></script>
+        <script type="text/javascript" src="js/plugins/morris/morris.min.js"></script>       
+        <script type="text/javascript" src="js/plugins/rickshaw/d3.v3.js"></script>
+        <script type="text/javascript" src="js/plugins/rickshaw/rickshaw.min.js"></script>
+        <script type='text/javascript' src='js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js'></script>
+        <script type='text/javascript' src='js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js'></script>                
+        <script type='text/javascript' src='js/plugins/bootstrap/bootstrap-datepicker.js'></script>                
+        <script type="text/javascript" src="js/plugins/owl/owl.carousel.min.js"></script>                 
+        
+        <script type="text/javascript" src="js/plugins/moment.min.js"></script>
+        <script type="text/javascript" src="js/plugins/daterangepicker/daterangepicker.js"></script>
+        <!-- END THIS PAGE PLUGINS-->        
+
+        <!-- START TEMPLATE -->
+        <script type="text/javascript" src="js/settings.js"></script>
+        
+        <script type="text/javascript" src="js/plugins.js"></script>        
+        <script type="text/javascript" src="js/actions.js"></script>
+        
+        <script type="text/javascript" src="js/demo_dashboard.js"></script>
+        <!-- END TEMPLATE -->
+    <!-- END SCRIPTS -->         
+    </body>
+</html>
+
+
+
+
+
+
